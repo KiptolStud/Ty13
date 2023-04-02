19e61242816dbdf725554d52cb019e2eb321bb00
KiptolStud <Kiptol@yandex.ru>
2023-04-02 19:22:43 +0300

    add Exam_Java (try1)

---
+++ Java_Home_Work/Java_Hw1/untitled/src/main/java/Exam_Java/ToysComparator.java
@@ -0,0 +1,14 @@
+package Exam_Java;
+
+import java.util.Comparator;
+
+public class ToysComparator implements Comparator<ToysShop> {
+    @Override
+    public int compare(ToysShop t1, ToysShop t2) {
+        if (t1.getChance() < t2.getChance())
+            return 1;
+        if (t1.getChance() > t2.getChance())
+            return -1;
+        return 0;
+    }
+}
